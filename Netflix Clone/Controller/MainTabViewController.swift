ca631632d07f6536331fa387c95b5a7c8ce7996f
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-08-11 23:20:58 +0530

    Folder Structure Updated & ViewController Added

--- Netflix Clone/Controller/MainTabViewController.swift
+++ Netflix Clone/Controller/MainTabViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
-
 }
-
